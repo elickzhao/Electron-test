37fd629904fda04ae20a78087f7456542a8d71c7
elickzhao <xwiwi@foxmail.com>
2017-06-18 01:05:22 +0800

    已经用了最新版,不过新版的运行稍微有些问题

--- build/dev-runner.js
+++ build/dev-runner.js
@@ -5,7 +5,7 @@
 
 /**
  * 简易版本的electron测试启动
- * 这个文件还有更新版明天可以看看
+ * 
  */
 
 //const config = require('../config')
@@ -18,55 +18,82 @@ const END = '\x1b[0m'
 
 let isElectronOpen = false
 
-function format (command, data, color) {
+function format(command, data, color) {
   return `${color + command + END}
     ${  // Two space offset
     data.toString().trim().replace(/\n/g, `\n${repeat(' ', command.length + 2)}`)
     }\n`
 }
 
-function repeat (str, times) {
+function repeat(str, times) {
   return (new Array(times + 1)).join(str)
 }
 
 const children = []
 
-function run (command, color, name) {
-  const child = exec(command)
+// function run (command, color, name) {
+//   const child = exec(command)
 
-  child.stdout.on('data', data => {
-    console.log(format(name, data, color))
+//   child.stdout.on('data', data => {
+//     console.log(format(name, data, color))
 
-    /**
-     * Start electron after VALID build
-     * (prevents electron from opening a blank window that requires refreshing)
-     *
-     * NOTE: needs more testing for stability
-     */
-    /* if (/VALID/g.test(data.toString().trim().replace(/\n/g, '\n' + repeat(' ', command.length + 2))) && !isElectronOpen) {
-      console.log(`${BLUE}Starting electron...\n${END}`)
-      run('cross-env NODE_ENV=development electron app/electron.js', BLUE, 'electron')
-      isElectronOpen = true
-    } */
-    if (/Compiled/g.test(data.toString().trim().replace(/\n/g, `\n${repeat(' ', command.length + 2)}`)) && !isElectronOpen) {
-      console.log(`${BLUE}Starting electron...\n${END}`)
-      run('cross-env NODE_ENV=development electron .', BLUE, 'electron')
-      isElectronOpen = true
-    }
+//     /**
+//      * Start electron after VALID build
+//      * (prevents electron from opening a blank window that requires refreshing)
+//      *
+//      * NOTE: needs more testing for stability
+//      */
+//     /* if (/VALID/g.test(data.toString().trim().replace(/\n/g, '\n' + repeat(' ', command.length + 2))) && !isElectronOpen) {
+//       console.log(`${BLUE}Starting electron...\n${END}`)
+//       run('cross-env NODE_ENV=development electron app/electron.js', BLUE, 'electron')
+//       isElectronOpen = true
+//     } */
+//     if (/Compiled/g.test(data.toString().trim().replace(/\n/g, `\n${repeat(' ', command.length + 2)}`)) && !isElectronOpen) {
+//       console.log(`${BLUE}Starting electron...\n${END}`)
+//       run('cross-env NODE_ENV=development electron .', BLUE, 'electron')
+//       isElectronOpen = true
+//     }
+//   })
+
+//   child.stderr.on('data', data => console.error(format(name, data, color)))
+//   child.on('exit', code => exit(code))
+
+//   children.push(child)
+// }
+
+function run(command, color) {
+  var child = exec(command)
+
+  child.stdout.on('data', function (data) {
+    console.log(format(command.split(' ')[2], data, color))
   })
 
-  child.stderr.on('data', data => console.error(format(name, data, color)))
-  child.on('exit', code => exit(code))
+  child.stderr.on('data', function (data) {
+    console.error(format(command.split(' ')[2], data, color))
+  })
+
+  child.on('exit', function (code) {
+    exit(code)
+  })
 
   children.push(child)
 }
 
-function exit (code) {
+function exit(code) {
   children.forEach(child => {
     treeKill(child.pid)
   })
 }
 
-console.log(`${YELLOW}Starting webpack-dev-server...\n${END}`)
-run(`webpack-dev-server --inline --hot --colors --config webpack.dev.config.js --content-base ./`, YELLOW, 'webpack-server-info:')
+// console.log(`${YELLOW}Starting webpack-dev-server...\n${END}`)
+// run(`webpack-dev-server --inline --hot --colors --config webpack.dev.config.js --content-base ./`, YELLOW, 'webpack-server-info:')
 //webpack-dev-server --content-base ./ --open  --hot --compress --history-api-fallback --config webpack.dev.config.js
+
+//这么写不行可能因为是启动浏览器的关系
+//run('npm run dev',YELLOW)
+//而且必须这么写
+//这两个run几乎是同时的,是不会等第一个完全执行完毕
+run(`webpack-dev-server --inline --hot --colors --config webpack.dev.config.js --content-base ./`, YELLOW)
+//console.log(children.length)
+run('cross-env NODE_ENV=development electron .', BLUE)
+//console.log(children.length)
